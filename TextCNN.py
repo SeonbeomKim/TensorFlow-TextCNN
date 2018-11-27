e38d71ff1a94d940c701e99c001fa826b621a266
Seonbeom Kim <kymj1@naver.com>
2018-11-27 14:56:31 +0900

    Add files via upload

--- TextCNN.py
+++ TextCNN.py
@@ -4,38 +4,59 @@ import numpy as np
 tf.set_random_seed(777)
 
 class TextCNN:
-	def __init__(self, sess, voca_size, embedding_mode, embedding_size, window_size, filters, num_classes, pad_idx, lr):
+	def __init__(self, sess, window_size, filters, num_classes, pad_idx, lr,
+					voca_size, embedding_size, embedding_mode='rand', word_embedding=None):
 		self.sess = sess
-		self.voca_size = voca_size
-		self.embedding_mode = embedding_mode # mode: rand static nonstatic multichannel
-		self.embedding_size = embedding_size
 		self.window_size = window_size # like [3, 4, 5]
 		self.filters = filters # the number of out filter
 		self.num_classes = num_classes
 		self.pad_idx = pad_idx
 		self.lr = lr
+		self.voca_size = voca_size
+		self.embedding_size = embedding_size
+		self.embedding_mode = embedding_mode # mode: rand static nonstatic multichannel
+		self.word_embedding = word_embedding # only if embedding_mode is not 'rand'
+
 
 		with tf.name_scope("placeholder"):
 			self.idx_input = tf.placeholder(tf.int32, [None, None], name="idx_input")
 			self.target = tf.placeholder(tf.int32, [None, self.num_classes], name="target")
 			self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 
+
 		with tf.name_scope("embedding_table"):
 			if self.embedding_mode == 'rand':
-				embedding_table = self.rand_embedding_table(self.voca_size, self.pad_idx, self.embedding_size)
-			else:
-				pass # 구현해야함.
+				self.embedding_table = self.rand_embedding_table(self.voca_size, self.pad_idx, self.embedding_size)
+			
+			elif self.embedding_mode == 'static':
+				self.embedding_table = tf.constant(self.word_embedding, dtype=tf.float32)
+			
+			elif self.embedding_mode == 'nonstatic':
+				self.embedding_table = tf.Variable(self.word_embedding, dtype=tf.float32) # trainable
+			
+			elif self.embedding_mode == 'multichannel':
+				self.static_embedding_table = tf.constant(self.word_embedding, dtype=tf.float32)
+				self.nonstatic_embedding_table = tf.Variable(self.word_embedding, dtype=tf.float32) # trainable
+				
 
 		with tf.name_scope("embedding_lookup"): 
-			embedding = tf.nn.embedding_lookup(embedding_table, self.idx_input) # [N, self.idx_input_length, self.embedding_size]
-			self.embedding = tf.expand_dims(embedding, axis=-1) # [N, self.idx_input_length, self.embedding_size, 1]
-			#multichannel이면 expand_dims 한것 두개 axis=-1에서 concat해야함 [N, self.idx_input_length, self.embedding_size, 2] 되도록 
+			if self.embedding_mode != 'multichannel':
+				embedding = tf.nn.embedding_lookup(self.embedding_table, self.idx_input) # [N, self.idx_input_length, self.embedding_size]
+				self.embedding = tf.expand_dims(embedding, axis=-1) # [N, self.idx_input_length, self.embedding_size, 1]
+			else:
+				static_embedding = tf.nn.embedding_lookup(self.static_embedding_table, self.idx_input) # [N, self.idx_input_length, self.embedding_size]
+				static_embedding = tf.expand_dims(static_embedding, axis=-1) # [N, self.idx_input_length, self.embedding_size, 1]
+				nonstatic_embedding = tf.nn.embedding_lookup(self.nonstatic_embedding_table, self.idx_input) # [N, self.idx_input_length, self.embedding_size]
+				nonstatic_embedding = tf.expand_dims(nonstatic_embedding, axis=-1) # [N, self.idx_input_length, self.embedding_size, 1]
+				self.embedding = tf.concat((static_embedding, nonstatic_embedding), axis=-1) # [N, self.idx_input_length, self.embedding_size, 2]
+			
 
 		with tf.name_scope("model_architecture"):
 			self.convolved_features = self.convolution(self.embedding, self.embedding_size, self.window_size, self.filters)
 			self.pooled_features = self.max_pooling(self.convolved_features)
 			self.concat_and_flatten_features = self.concat_and_flatten(self.pooled_features)
 			self.pred = self.dropout_and_dense(self.concat_and_flatten_features, self.num_classes, self.keep_prob)
+
 
 		with tf.name_scope('train'): 
 			# calc train_cost
@@ -55,6 +76,7 @@ class TextCNN:
 		embedding_table = tf.concat((front, zero, end), axis=0)
 		return embedding_table
 
+
 	def convolution(self, embedding, embedding_size, window_size, filters):
 		convolved_features = []
 		for window in window_size:
@@ -70,6 +92,7 @@ class TextCNN:
 			convolved_features.append(convolved) # [N, ?, 1, filters] 이 len(window_size) 만큼 존재.
 		return convolved_features
 
+
 	def max_pooling(self, convolved_features):
 		pooled_features = []
 		for convolved in convolved_features: # [N, ?, 1, self.filters]
@@ -81,10 +104,12 @@ class TextCNN:
 			pooled_features.append(max_pool) # [N, 1, 1, self.filters] 이 len(window_size) 만큼 존재.
 		return pooled_features
 
+
 	def concat_and_flatten(self, pooled_features):
 		concat = tf.concat(pooled_features, axis=-1) # [N, 1, 1, self.filters*len(self.window_size)]
 		concat_and_flatten_features = tf.layers.flatten(concat) # [N, self.filters*len(self.window_size)]
 		return concat_and_flatten_features
+
 
 	def dropout_and_dense(self, concat_and_flatten_features, num_classes, keep_prob):
 		dropout = tf.nn.dropout(concat_and_flatten_features, keep_prob = keep_prob)
--- test.py
+++ test.py
@@ -2,9 +2,60 @@ import tensorflow as tf
 import numpy as np
 import textcnn
 
+np.random.seed(777)
+
+
+def testcode1():
+	print(sess.run(model.embedding, {model.idx_input:idx}))
+
+	zz = model.convolved_features
+	#print(sess.run(zz, {model.idx_input:idx}))
+	print(zz[0])
+	print(zz[1])
+	print(zz[2])
+	print(sess.run(zz[0], {model.idx_input:idx}))
+
+	aa0 = sess.run(zz[0], {model.idx_input:idx})
+	aa1 = sess.run(zz[1], {model.idx_input:idx})
+	aa2 = sess.run(zz[2], {model.idx_input:idx})
+
+	print(aa0.shape, aa1.shape, aa2.shape)
+
+	qq0 = sess.run(model.pooled_features[0], {model.idx_input:idx})
+	qq1 = sess.run(model.pooled_features[1], {model.idx_input:idx})
+	qq2 = sess.run(model.pooled_features[2], {model.idx_input:idx})
+
+	print(qq0.shape, qq1.shape, qq2.shape)
+	#print(aa0,'\n', qq0, '\n\n')
+	#print(aa1,'\n', qq1, '\n\n')
+	#print(aa2,'\n', qq2, '\n\n')
+
+	print(qq0,'\n')
+	print(qq1,'\n')
+	print(qq2,'\n')
+
+	xx = sess.run(model.concat_and_flatten_features, {model.idx_input:idx})
+	print(xx, xx.shape)
+
+	pred = sess.run(model.pred, {model.idx_input:idx, model.keep_prob:1})
+	print(pred, pred.shape)
+
+	cost = sess.run(model.train_cost, {model.idx_input:idx, model.target:target, model.keep_prob:1})
+	print(cost)
+
+
+	for i in range(1,20*3):
+		cost , _, pred= sess.run([model.train_cost, model.minimize, tf.nn.softmax(model.pred, dim=1)], {model.idx_input:idx, model.target:target, model.keep_prob:1})
+		print(i, cost, pred)
+	#pred = sess.run(tf.nn.softmax(model.pred, dim=1), {model.idx_input:idx, model.keep_prob:1})
+	#print(pred)
+
+
+
+
 voca_size = 10
-embedding_mode='rand'
-embedding_size = 4
+embedding_mode='multichannel'#'nonstatic'#'static'#'rand'
+embedding_size = 3
 window_size = [3,4,5]
 filters=2
 num_classes = 2
@@ -13,59 +64,27 @@ lr = 0.01
 idx = np.array([[1,2,3,4,5], [1,2,3,0,0], [2,3,0,0,0]])
 target = np.array([[1,0], [1,0], [0,1]])
 
+word2vec = np.random.randn(voca_size, embedding_size)
+print(word2vec, word2vec.shape)
+
 sess = tf.Session()
 model = textcnn.TextCNN(
 		sess=sess, 
-		voca_size=voca_size, 
-		embedding_mode=embedding_mode, 
-		embedding_size=embedding_size, 
 		window_size=window_size, 
 		filters=filters,
 		num_classes=num_classes, 
 		pad_idx=pad_idx, 
-		lr=lr
+		lr=lr,
+		voca_size=voca_size, 
+		embedding_size=embedding_size, 
+		embedding_mode=embedding_mode, 
+		word_embedding=word2vec # if embedding_mode is not 'rand'
 	)
 
-print(sess.run(model.embedding, {model.idx_input:idx}))
+testcode1()
+#print(sess.run(model.embedding_table))
+print(sess.run(model.static_embedding_table))
+print(sess.run(model.nonstatic_embedding_table))
+print(word2vec, word2vec.shape)
 
-zz = model.convolved_features
-#print(sess.run(zz, {model.idx_input:idx}))
-print(zz[0])
-print(zz[1])
-print(zz[2])
-print(sess.run(zz[0], {model.idx_input:idx}))
-
-aa0 = sess.run(zz[0], {model.idx_input:idx})
-aa1 = sess.run(zz[1], {model.idx_input:idx})
-aa2 = sess.run(zz[2], {model.idx_input:idx})
-
-print(aa0.shape, aa1.shape, aa2.shape)
-
-qq0 = sess.run(model.pooled_features[0], {model.idx_input:idx})
-qq1 = sess.run(model.pooled_features[1], {model.idx_input:idx})
-qq2 = sess.run(model.pooled_features[2], {model.idx_input:idx})
-
-print(qq0.shape, qq1.shape, qq2.shape)
-#print(aa0,'\n', qq0, '\n\n')
-#print(aa1,'\n', qq1, '\n\n')
-#print(aa2,'\n', qq2, '\n\n')
-
-print(qq0,'\n')
-print(qq1,'\n')
-print(qq2,'\n')
-
-xx = sess.run(model.concat_and_flatten_features, {model.idx_input:idx})
-print(xx, xx.shape)
-
-pred = sess.run(model.pred, {model.idx_input:idx, model.keep_prob:1})
-print(pred, pred.shape)
-
-cost = sess.run(model.train_cost, {model.idx_input:idx, model.target:target, model.keep_prob:1})
-print(cost)
-
-
-for i in range(1,100):
-	cost , _, pred= sess.run([model.train_cost, model.minimize, tf.nn.softmax(model.pred, dim=1)], {model.idx_input:idx, model.target:target, model.keep_prob:1})
-	print(i, cost, pred)
-#pred = sess.run(tf.nn.softmax(model.pred, dim=1), {model.idx_input:idx, model.keep_prob:1})
-#print(pred)
+#print(model.embedding)
